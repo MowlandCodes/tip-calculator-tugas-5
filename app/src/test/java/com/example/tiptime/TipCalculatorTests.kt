9032f83e1b0d8f397c07b3cab7a869d0cca18b63
MowlandCodes <mowlandcodes@aol.com>
2026-05-16 08:16:43 +0700

    test: added another test and docs

--- app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
+++ app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
@@ -13,4 +13,13 @@ class TipCalculatorTests {
         val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, false)
         assertEquals(expectedTip, actualTip)
     }
+
+    @Test
+    fun calculateTip_20PercentRoundup(){
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, true)
+        assertEquals(expectedTip, actualTip)
+    }
 }
